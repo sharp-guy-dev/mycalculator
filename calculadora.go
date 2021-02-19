e029a97020141323227e1257a1fa981e291d497d
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-02-19 09:03:22 -0600

    my first calculator

---
+++ calculadora.go
@@ -0,0 +1,49 @@
+package mycalculator
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct{}
+
+func (calc) operate(entrada string, operador string) int {
+	entradaLimpia := strings.Split(entrada, operador)
+
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+
+	switch operador {
+	case "+":
+		return operador1 + operador2
+	case "-":
+		return operador1 - operador2
+	case "*":
+		return operador1 * operador2
+	case "/":
+		return operador1 / operador2
+	default:
+		return 0
+	}
+}
+
+func parsear(entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func main() {
+	entrada := leerEntrada()
+	operador := leerEntrada()
+	c := calc{}
+	fmt.Println(c.operate(entrada, operador))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/whiteagleinc/mycalculator
+
+go 1.15
